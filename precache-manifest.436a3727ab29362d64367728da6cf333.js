3f4f47953d0146cf46c1dcb59a65001ffafef7f8
Benny <bennyhinnotieno@gmail.com>
2020-09-18 23:36:39 +0300

    Updates

---
+++ precache-manifest.436a3727ab29362d64367728da6cf333.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6859bb0858f3377e48c532ebab9644c",
+    "url": "/Recipe-Finder-App/index.html"
+  },
+  {
+    "revision": "b1fb16177938a899a205",
+    "url": "/Recipe-Finder-App/static/css/main.d53fbe70.chunk.css"
+  },
+  {
+    "revision": "ea43401e5c8c9d38df5a",
+    "url": "/Recipe-Finder-App/static/js/2.e049d543.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipe-Finder-App/static/js/2.e049d543.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1fb16177938a899a205",
+    "url": "/Recipe-Finder-App/static/js/main.1a89cc8b.chunk.js"
+  },
+  {
+    "revision": "bd7baee0a1314fcd31cf",
+    "url": "/Recipe-Finder-App/static/js/runtime-main.93f89268.js"
+  }
+]);
